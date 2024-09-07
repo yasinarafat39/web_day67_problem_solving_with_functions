b9475fb3dd02019e482936a72342a017835f8cc4
Yasin Arafat <yasinarafat2557@gmail.com>
2024-09-07 14:36:19 +0600

    Math.property, Math Object practce

---
+++ math.js
@@ -0,0 +1,15 @@
+console.log(Math.min(21, 54, 65, 87, 45)); // 21
+console.log(Math.max(21, 54, 65, 87, 45)); // 87
+console.log(Math.PI); // 3.141592653589793
+console.log(Math.abs(12 - 452)); // 440
+console.log(Math.round(2.25)); // 2
+console.log(Math.round(2.8)); // 3
+console.log(Math.round(2.51)); // 3
+console.log(Math.floor(2.8)); // 2
+console.log(Math.ceil(2.1)); // 3
+console.log(Math.round(Math.random() * 100));  
+console.log(Math.pow(10, 2)); // 100
+console.log(Math.pow(5, 2)); // 25
+console.log(Math.sqrt(64)); // 8
+
+
